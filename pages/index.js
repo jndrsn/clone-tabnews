45c128001ad219d0e984bae22b757db53a5ceca6
Janderson Lopes <42125347+jndrsn@users.noreply.github.com>
2024-08-02 21:21:44 +0000

    atualiza menssagem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-  return (
-    <h1>Oi amor, amo muito você! teste para ver se atualiza ao mesmo tempo</h1>
-  );
+  return <h1>Oi amor, amo muito você!</h1>;
 }
 
 function teste() {
